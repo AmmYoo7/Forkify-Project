f18ad6a79e89e59253ef585015218b1b915f621c
AmmYoo7 <rajamrit175@gmail.com>
2023-10-23 13:47:37 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "1146b008-7576-4c9f-a75a-58593fe3bdb1";
+export const MODAL_CLOSE_SEC = 2.5;
